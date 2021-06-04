caf5864a71f162fdfd0b22eb04de1c22a2af52dd
vaismann <vassiliartemjev@gmail.com>
2021-06-04 19:05:19 +0300

    module2-task1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function getRandomInt(min, max) {
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+
+  if (min >= max) {
+    console.log('min value cannot be bigger or same than max');
+  } else {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+console.log(getRandomInt(0, 100));
+
+function getRandomFloatInt(min, max, fixed) {
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  if (min >= max) {
+    console.log('min value cannot be bigger or same than max');
+  } else {
+      return (Math.random() * (max - min + 1)) + min;
+    }
+}
+console.log(getRandomFloatInt(0, 100, 10));
