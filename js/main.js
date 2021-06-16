1d63e00ec6d0da380705021235f7e31f4ed64e20
vaismann <vassiliartemjev@gmail.com>
2021-06-16 23:37:43 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,59 +1,80 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min >= max) {
-    return('min value cannot be bigger or same than max');
-  } else if (min < 0) {
-    return('number cannot be negative');
-  } else {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-}
-getRandomInt(0, 100);
-
-function getRandomFloatInt(min, max, fixed) {
-  if (min >= max) {
-    return('min value cannot be bigger or same than max');
-  } else if ( min < 0 || fixed < 0) {
-    return('number cannot be negative');
-  } else {
-    return ((Math.random() * (max - min + 1)) + min).toFixed(fixed);
-  }
-}
-getRandomFloatInt(0, 100, 10);
-
-const ACCOMODATION_TYPE = ['palace', 'flat', 'house', 'bungalow','hotel'];
-const TIME_LIST = ['12:00', '13:00', '14:00'];
-const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTO_LIST = [
+const COUNT_OFFERS = 10;
+const TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const IMAGES = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+const ACC_TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomInt(0, elements.length - 1)];
+function getRandomPositiveFloat (a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+const getNumber = function () {
+  const result = getRandomPositiveInteger(1, 10);
+  return result < 10 ? `0${result}` : result;
 };
 
-const generateObject = () => {
+const getLocation = function () {
   return {
-  const author = {
-    avatar: 'img/avatars/user[{{xx}}].png',
+    lat: getRandomPositiveFloat(35.65000, 35.70000, 5),
+    lng: getRandomPositiveFloat(139.70000, 139.80000, 5),
   };
+};
 
-  const offer = {
-    title: 'Title',
-    location: [{{location.lat}}, {{location.lng}}],
-    price: getRandomInt(0, 100000),
-    type: getRandomArrayElement(ACCOMODATION_TYPE),
-    rooms: getRandomInt(),
-    guests: getRandomInt(),
-    checkin: getRandomArrayElement(TIME),
-    checkout: getRandomArrayElement(TIME),
-    features:
+const getRandomItem = function (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+};
 
+const getRandomArray = function (array) {
+  const result = [];
+  const randomInt = getRandomPositiveInteger(0, array.length - 1);
+  for (let el = 0; el <= randomInt; el++) {
+    const item = getRandomItem(array);
+    if (result.includes(item)) { continue; }
+    result.push(item);
+  }
+  return result;
+};
 
+const createOffer = function () {
+  const location = getLocation();
+
+  return {
+    author: {
+      avatar: `img/avatars/user${getNumber()}.png`,
+    },
+    offer: {
+      title: 'Title',
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomPositiveInteger(1, 100000),
+      rooms: getRandomPositiveInteger(1, 20),
+      type: getRandomItem(ACC_TYPE),
+      guests: getRandomPositiveInteger(1, 100),
+      checkin: getRandomItem(TIMES),
+      checkout: getRandomItem(TIMES),
+      features: getRandomArray(FEATURES),
+      description: 'Description',
+      photos: getRandomArray(IMAGES),
+    },
+    location,
   };
-}
-}
+};
+
+const getOffers = function (count) {
+  return new Array(count).fill(null).map(() => createOffer());
+};
+
+getOffers(COUNT_OFFERS);
