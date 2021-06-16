8d621f4875b4bd65ed0c8b4aa864ebc74d081389
vaismann <vaismann@icloud.com>
2021-06-16 10:23:18 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -22,3 +22,38 @@ function getRandomFloatInt(min, max, fixed) {
   }
 }
 getRandomFloatInt(0, 100, 10);
+
+const ACCOMODATION_TYPE = ['palace', 'flat', 'house', 'bungalow','hotel'];
+const TIME_LIST = ['12:00', '13:00', '14:00'];
+const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTO_LIST = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInt(0, elements.length - 1)];
+};
+
+const generateObject = () => {
+  return {
+  const author = {
+    avatar: 'img/avatars/user[{{xx}}].png',
+  };
+
+  const offer = {
+    title: 'Title',
+    location: [{{location.lat}}, {{location.lng}}],
+    price: getRandomInt(0, 100000),
+    type: getRandomArrayElement(ACCOMODATION_TYPE),
+    rooms: getRandomInt(),
+    guests: getRandomInt(),
+    checkin: getRandomArrayElement(TIME),
+    checkout: getRandomArrayElement(TIME),
+    features:
+
+
+  };
+}
+}
